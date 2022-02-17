a41238d8d6ecef2d950990f66019b0266631cd37
woonees2 <hello_world_@kakao.com>
2022-02-17 15:08:11 +0900

    Add color palette

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      mainRed: "#E55132",
+      mainOrange: "#EF9734",
+      mainYellow: "#F1BD3D",
+      mainLightYellow: "#F2C744",
+    },
+    secondary: {
+      mainWhite: "#FEFDFC",
+      mainBlack: "#191919",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
