ec16e60fe70b02c8482736edc6f16323ca43ccf1
woonees2 <hello_world_@kakao.com>
2022-02-22 20:08:57 +0900

    Update route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { DetailPage } from "./pages/DetailPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SearchPage from "./pages/SearchPage";
+
 import GlobalStyle from "./design/GlobalStyles";
 function App() {
   return (
@@ -7,11 +8,10 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <header className="App-header"></header>
-
         <Routes>
           <Route exact path="/" element={<IntroPage />}></Route>
           <Route exact path="/search" element={<SearchPage />}></Route>
-          <Route path="/liquor/" element={<DetailPage />}></Route>
+          <Route path="/liquor" element={<DetailPage />}></Route>
           <Route exact path="/sing-up" element={<SignUpPage />}></Route>
           <Route exact path="/sing-in" element={<SignInPage />}></Route>
           <Route path="*" element={<ErrorPage />}></Route>
