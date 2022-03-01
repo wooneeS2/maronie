6dfc81e01ff89a94aaa512790740807efaa16b0f
julia98percent <ji_0315@naver.com>
2022-03-01 22:48:40 +0900

    Fix Tabs to component

--- src/pages/TextSearchResultPage.jsx
+++ src/pages/TextSearchResultPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Tab, Tabs } from "@mui/material";
+// import { Tab, Tabs } from "@mui/material";
+import MenuTabs from "../components/SearchPage/MenuTabs";
 import TextSearchInput from "../components/SearchPage/TextSearchInput";
 import { TextSearchResultWrapper } from "../design/SearchPage/SearchPageStyles";
 import {
@@ -14,40 +15,48 @@ import { AiOutlineRight } from "react-icons/ai";
 function TextSearchResultPage() {
   const params = useParams();
   const searchKeyword = params.keyword;
+  const [currentTab, setCurrentTab] = React.useState("liquor");
   let dummy = {
     liquor: [
       {
-        name: "술이름1",
+        id: 1,
+        liquor_name: "술이름1",
         price: 100,
-        url: "https://products0.imgix.drizly.com/ci-bombay-sapphire-4967085f606d9efa.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&dpr=2&fm=jpg&h=240&q=20",
+        image_path:
+          "https://products0.imgix.drizly.com/ci-bombay-sapphire-4967085f606d9efa.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&dpr=2&fm=jpg&h=240&q=20",
         description:
           "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, nulla voluptas necessitatibus odit nemo odio ullam impedit velit iste facere cum accusantium beatae perferendis consectetur quia repellendus minima veniam quibusdam.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, nulla voluptas necessitatibus odit nemo odio ullam impedit velit iste facere cum accusantium beatae perferendis consectetur quia repellendus minima veniam quibusdam.(약 470자)",
+        rating: 3.5,
       },
       {
-        name: "술이름2",
-        price: 100,
-        url: "https://products0.imgix.drizly.com/ci-bombay-sapphire-4967085f606d9efa.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&dpr=2&fm=jpg&h=240&q=20",
+        id: 2,
+        liquor_name: "술이름2",
+        price: 1000,
+        image_path:
+          "https://products0.imgix.drizly.com/ci-bombay-sapphire-4967085f606d9efa.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&dpr=2&fm=jpg&h=240&q=20",
         description:
           "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, nulla voluptas necessitatibus odit nemo odio ullam impedit velit iste facere cum accusantium beatae perferendis consectetur quia repellendus minima veniam quibusdam.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, nulla voluptas necessitatibus odit nemo odio ull(약 350자)",
+        rating: 4.5,
       },
       {
+        id: 3,
         name: "술이름3",
         price: 100,
-        url: "https://products0.imgix.drizly.com/ci-bombay-sapphire-4967085f606d9efa.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&dpr=2&fm=jpg&h=240&q=20",
+        image_path:
+          "https://products0.imgix.drizly.com/ci-bombay-sapphire-4967085f606d9efa.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&dpr=2&fm=jpg&h=240&q=20",
         description:
           "Lorem ipsum dolor sit, amet consectetur adipi Lorem ipsum dolor sit, amet consectetur adipi Lorem ipsum dolor sit, amet consectetur adipi a;jj(약 150자)",
-      },
-      {
-        name: "술이름4",
-        price: 100,
-        url: "https://products0.imgix.drizly.com/ci-bombay-sapphire-4967085f606d9efa.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&dpr=2&fm=jpg&h=240&q=20",
+        rating: 2.0,
       },
     ],
     cocktail: [
       {
-        name: "칵테일1",
+        cocktail_id: 1,
+        cocktail_name: "칵테일1",
         price: 1100000,
-        url: "http://res.heraldm.com/content/image/2015/03/12/20150312001242_0.jpg",
+        image_path:
+          "http://res.heraldm.com/content/image/2015/03/12/20150312001242_0.jpg",
+        description: "음~ 맛있다",
       },
     ],
   };
@@ -65,18 +74,11 @@ function TextSearchResultPage() {
     );
   });
 
-  const [currentTab, setCurrentTab] = React.useState("liquor");
-  const handleChange = (e, newValue) => {
-    setCurrentTab(newValue);
-  };
   return (
     <>
       {/* TODO 폰트크기 키우기 */}
       <TextSearchInput />
-      <Tabs variant="fullWidth" value={currentTab} onChange={handleChange}>
-        <Tab value="liquor" label="술" />
-        <Tab value="cocktail" label="칵테일" />
-      </Tabs>
+      <MenuTabs currentTab={currentTab} setCurrentTab={setCurrentTab} />
 
       {dummy[currentTab].length > 0 ? (
         <TextSearchResultWrapper className="contentArea">
