fabc5ca74b558caf387cf56509d2096d0f46c963
woonees2 <hello_world_@kakao.com>
2022-03-11 14:01:47 +0900

    Add review more see

--- src/design/detailPage/LiquorReviewStyles.jsx
+++ src/design/detailPage/LiquorReviewStyles.jsx
@@ -14,6 +14,7 @@ export const MoreReviewButton = styled.div`
   justify-content: center;
   align-item: center;
   margin: 0 auto;
+  margin-top: 1rem;
   &:hover {
     box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
       rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
