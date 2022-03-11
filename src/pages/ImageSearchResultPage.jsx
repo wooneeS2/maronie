85dfed1a53db931abc9cbf0ae0675f7b5e4a10eb
woonees2 <hello_world_@kakao.com>
2022-03-12 01:21:26 +0900

    Update image search result design

--- src/pages/ImageSearchResultPage.jsx
+++ src/pages/ImageSearchResultPage.jsx
@@ -6,13 +6,10 @@ import {
   ImageSearchImg,
   ImageSearchTitle,
   ImageResultWrapper,
+  MoreButton,
 } from "../design/SearchResultPage/ImageSearchResultStyles";
-import {
-  FlexRowCenterBox,
-  FlexColumnCenterBox,
-  StyledButton,
-  StyledLink,
-} from "../design/commonStyles";
+import { FlexRowCenterBox, StyledLink } from "../design/commonStyles";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function ImageSearchResultPage() {
   const [searchImage, setSearchImage] = useRecoilState(searchImageState);
@@ -35,6 +32,8 @@ function ImageSearchResultPage() {
                 src={searchImage}
                 alt="이미지 검색 대상"
               />
+              <ArrowForwardIosIcon style={{ padding: "2rem" }} />
+
               <ImageSearchImg
                 src={process.env.REACT_APP_DB_IMG + resultImage["image_path"]}
                 alt="결과 이미지"
@@ -42,25 +41,30 @@ function ImageSearchResultPage() {
             </FlexRowCenterBox>
             <ImageSearchTitle>
               이 술은{" "}
-              <StyledLink
-                style={{ textDecoration: "underline" }}
-                to={`/liquor/${resultImage["liquor_id"]}`}
+              <span
+                style={{
+                  background:
+                    "linear-gradient(to top, #F5C085 95%, transparent 50%)",
+                }}
               >
                 {resultImage["liquor_name_kor"]}({resultImage["liquor_name"]})
-              </StyledLink>
+              </span>{" "}
               입니다!
             </ImageSearchTitle>
-            <FlexColumnCenterBox>
-              <p style={{ marginBottom: "1px" }}>찾는 술이 아니신가요?</p>
-            </FlexColumnCenterBox>
+            <StyledLink
+              style={{ textDecoration: "underline" }}
+              to={`/liquor/${resultImage["liquor_id"]}`}
+            >
+              <MoreButton>상세정보 보러 가기</MoreButton>
+            </StyledLink>
           </>
         ) : (
           <NoImageSearchResult />
         )}
         <StyledLink to="/search">
-          <StyledButton onClick={() => resetSearchResult}>
+          <MoreButton onClick={() => resetSearchResult}>
             검색 페이지로 돌아가기
-          </StyledButton>
+          </MoreButton>
         </StyledLink>
       </ImageResultWrapper>
     </>
